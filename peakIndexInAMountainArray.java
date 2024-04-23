b18e28bf38ce6b9290dc494d4e80f1982ef4f188
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-04-23 21:07:39 +0530

    binary search

---
+++ peakIndexInAMountainArray.java
@@ -0,0 +1,30 @@
+// sorted array apply binary search
+// https://leetcode.com/problems/peak-index-in-a-mountain-array/description/ 
+
+// leetCode 852
+
+public class peakIndexInAMountainArray {
+    public static void main(String[] args) {
+
+    }
+
+    public int peakIndexInMountainArray(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] > arr[mid + 1]) {
+                // your are decreasing path of an array
+                // this is why end = mid -1;
+                end = mid;
+            } else {
+                // ascending part of an array
+                start = mid + 1;
+            }
+        }
+        // / start is == end and pointing largest element
+        // start and end are always used to find largest elements
+        return start;
+    }
+}
